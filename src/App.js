5bc07eeb3c8b4a85e92f9ae868ccad866c662405
Allie Robinson <allierob93@gmail.com>
2023-01-10 23:50:41 -0600

    :art: idk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { Video } from "./Video";
+import ReactPlayer from "react-player";
 
 function App() {
+  const width = 500;
+  const height = width * (9 / 16);
+
   return (
-    <div>
-      <Video />
-    </div>
+    <ReactPlayer
+      className="position-absolute"
+      url="https://www.youtube.com/watch?v=2xUKzeflvR0"
+      loop
+      playing
+      controls
+      volume={0.5}
+      width={width}
+      height={height}
+    />
   );
 }
 
